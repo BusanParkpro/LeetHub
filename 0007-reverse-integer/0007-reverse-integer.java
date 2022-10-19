7c499fe76adc5b0f38e67c260e14132507ae3ca6
DongJaePark <103563454+BusanParkpro@users.noreply.github.com>
2022-10-19 23:46:16 +0900

    Time: 6 ms (11.59%), Space: 42.3 MB (7.06%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int reverse(int x) {
+        
+        String str = String.valueOf(x);
+        String resultStr ="";
+        
+        String answer ="";
+        
+        boolean minusflag = false;
+        
+        if(str.charAt(0) == '-') {
+            minusflag = true;
+            str = str.substring(1);
+        }
+        
+        for(int i=str.length()-1; i >=0; i--){
+            resultStr += String.valueOf(str.charAt(i));
+        }
+        
+        Long resultLong = Long.parseLong(resultStr);
+        if(minusflag) resultLong *= -1;
+        
+        if(resultLong > Integer.MAX_VALUE || resultLong < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return resultLong.intValue();
+    }
+}
